ac6fde940d8378f962a00b869a47076f86d651d2
Your Name <you@example.com>
2024-04-17 18:34:41 +0100

    Commited

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer () { 
+    return (
+        <footer>
+
+            <p>&copy;{new Date().getFullYear()}Your website name</p>
+
+        </footer>
+    );
+
+}
+
+export default Footer
---
+++ src/components/Hometext.js
@@ -0,0 +1,15 @@
+function Hometext() {
+
+    const food1 = "Orange";
+    const food2 = "Banana";
+
+    return (
+        <ul>
+            <li> Apple </li>
+            <li>{food2}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    );
+
+}
+export default Hometext
