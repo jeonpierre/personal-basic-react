96b03aca3b4d1c2682339f29d9426d75ee8bdf31
Your Name <you@example.com>
2024-04-18 12:34:11 +0100

    Navbar Changes

--- src/Navbar index.js
+++ src/Navbar index.js
@@ -1,11 +1,26 @@
 import './Navbar index.scss';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 
 const Navbar = () => (
     <div className= 'nav-bar'>
-        <Link className='logo' to= '/'>
-        </Link>
+        <nav >
+            <div class="name">Jean-Pierre Viljoen</div>
+            <NavLink exact = 'True' activeclassname='active' className= 'about-link' to= '/About' >
+                <p1>About</p1>
+                 </NavLink>
+             <NavLink exact = 'True' activeclassname='active' className= 'about-link' to= '/Expereince' >
+                <p1>Experience</p1>
+                </NavLink>
+
+            <NavLink exact = 'True' activeclassname='active' className= 'about-link' to= '/Projects' >
+                 <p1>Projects</p1>
+                </NavLink>
+
+            <NavLink exact = 'True' activeclassname='active' className= 'about-link' to= '/Contact' >
+                 <p1>Contact</p1>
+                </NavLink>
+        </nav>
     </div>
 )
 
