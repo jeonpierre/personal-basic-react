f72e9795e8200056cb629c6b38c5adfeb1a464ed
Your Name <you@example.com>
2024-04-18 16:22:42 +0100

    Justified Nav bar and Split into left and right!

--- src/Navbar index.js
+++ src/Navbar index.js
@@ -5,10 +5,19 @@ import { Link, NavLink } from 'react-router-dom';
 const Navbar = () => (
     <div className= 'nav-bar'>
         <nav >
-            <div class="name">Jean-Pierre Viljoen</div>
+            <div class="nav links nav-links-left">
+                <div class='name'>
+                  <NavLink exact = 'True' activeclassname= 'active' className= 'about-link' to = '/'>
+                    Jean-Pierre Viljoen
+                  </NavLink>
+                </div>
+            </div> 
+            <div className="nav-links nav-links-right">
+
             <NavLink exact = 'True' activeclassname='active' className= 'about-link' to= '/About' >
                 <p1>About</p1>
                  </NavLink>
+
              <NavLink exact = 'True' activeclassname='active' className= 'about-link' to= '/Expereince' >
                 <p1>Experience</p1>
                 </NavLink>
@@ -20,6 +29,8 @@ const Navbar = () => (
             <NavLink exact = 'True' activeclassname='active' className= 'about-link' to= '/Contact' >
                  <p1>Contact</p1>
                 </NavLink>
+
+            </div>
         </nav>
     </div>
 )
